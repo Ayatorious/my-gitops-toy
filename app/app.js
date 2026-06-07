beaaac92ddab68265e8adb086cfbe4f6d134408c
Ayatorious <kanekijava@gmail.com>
2026-06-07 22:19:06 +0900

    Add app and GitHub Actions workflow for CI

---
+++ app/app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from my first container! v1\n');
+});
+
+server.listen(8080, () => {
+  console.log('Server starting on :8080');
+});
